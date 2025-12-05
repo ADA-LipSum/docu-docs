ac4ba34930599eb8cd954475d1e3fc96a92b9080
rlaxogh76 <btm.email2769@gmail.com>
2025-12-06 01:21:08 +0900

    feat: 사용자 정보 추가

---
+++ src/components/users.ts
@@ -0,0 +1,12 @@
+export const users = {
+    rlaxogh76: {
+        name: '김태호',
+        image: 'https://avatars.githubusercontent.com/u/108007761?v=4',
+        github_url: 'https://github.com/rlaxogh76',
+    },
+    Qlellow: {
+        name: '박창연',
+        image: 'https://avatars.githubusercontent.com/u/140193710?v=4',
+        github_url: 'https://github.com/Qlellow',
+    },
+};
